e217daae85fb7f85da48d37b0395f50b8ea9ba06
TabsMacedo <tabata.macedo1897@gmail.com>
2026-08-25 17:11:57 -0300

    correção do css

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,18 @@
 const menuToggle = document.getElementById("menuToggle");
-const menuToggleIcon = document.getElementById("menu__toggle-icon");
-const menuNav = document.getElementById(".menu");
-const menuLinks = document.querySelectorAll("navbar__item");
+const menuToggleIcon = document.getElementById("menuToggleIcon");
+const menuNav = document.getElementById("menuNavbar");
 
 function openMenu () {
-    menuToggle.setAttribute("aria-expanded", "true")
+    menuToggle.setAttribute("aria-expanded", "true");
     menuToggleIcon.src = "./assets/shared/icon-close.svg";
-    menuToggle.setAttribute("aria-label", "Fechar Menu")
+    menuToggle.setAttribute("aria-label", "Fechar menu");
+    menuNav.classList.add("menu__navbar--open");
 }
 function closeMenu () {    
-    menuToggle.setAttribute("aria-expanded", "false")
+    menuToggle.setAttribute("aria-expanded", "false");
     menuToggleIcon.src = "./assets/shared/icon-hamburger.svg";
-    menuToggle.setAttribute("aria-label", "Abrir Menu")
+    menuToggle.setAttribute("aria-label", "Abrir menu");
+    menuNav.classList.remove("menu__navbar--open");
 }
 
 menuToggle.addEventListener("click", function() {
