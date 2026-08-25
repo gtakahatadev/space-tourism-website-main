f6bb4a3db6c0d4338d7e0b11a79a26528d27553c
gtakahatadev <gabrieltakahata.dev@gmail.com>
2026-08-25 16:55:56 -0300

    adicionando javascript

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,24 @@
 const menuToggle = document.getElementById("menuToggle");
 const menuToggleIcon = document.getElementById("menu__toggle-icon");
+const menuNav = document.getElementById(".menu");
+const menuLinks = document.querySelectorAll("navbar__item");
+
+function openMenu () {
+    menuToggle.setAttribute("aria-expanded", "true")
+    menuToggleIcon.src = "./assets/shared/icon-close.svg";
+    menuToggle.setAttribute("aria-label", "Fechar Menu")
+}
+function closeMenu () {    
+    menuToggle.setAttribute("aria-expanded", "false")
+    menuToggleIcon.src = "./assets/shared/icon-hamburger.svg";
+    menuToggle.setAttribute("aria-label", "Abrir Menu")
+}
+
+menuToggle.addEventListener("click", function() {
+    const expanded = menuToggle.getAttribute("aria-expanded");
+    if (expanded === "false") {
+        openMenu()
+    } else {
+        closeMenu()
+    }
+})
