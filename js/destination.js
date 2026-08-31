3a92b3af662b51c45b0f5a0cfcde2678c55beaed
gtakahatadev <gabrieltakahata.dev@gmail.com>
2026-08-31 17:26:40 -0300

    adiciona css mobile

--- js/destination.js
+++ js/destination.js
@@ -9,23 +9,23 @@ const destinations = {
     mars: {
         name: "Mars",
         webp: "./assets/destination/image-mars.webp",
-        description: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-        distance: "384,400 km",
-        travel: "3 days",
+        description: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+        distance: "225 MIL. km",
+        travel: "9 months",
     },
     europa: {
         name: "Europa",
         webp: "./assets/destination/image-europa.webp",
-        description: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-        distance: "384,400 km",
-        travel: "3 days",
+        description: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+        distance: "628 MIL. km",
+        travel: "3 years",
     },
     titan: {
         name: "Titan",
         webp: "./assets/destination/image-titan.webp",
-        description: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-        distance: "384,400 km",
-        travel: "3 days",
+        description: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+        distance: "1.6 BIL. km",
+        travel: "7 years",
     }
 }
 
--- js/menu.js
+++ js/menu.js
@@ -2,20 +2,20 @@ const menuToggle = document.getElementById("menuToggle");
 const menuToggleIcon = document.getElementById("menuToggleIcon");
 const menuNav = document.getElementById("menuNavbar");
 
-function openMenu () {
+function openMenu() {
     menuToggle.setAttribute("aria-expanded", "true");
     menuToggleIcon.src = "./assets/shared/icon-close.svg";
     menuToggle.setAttribute("aria-label", "Fechar menu");
     menuNav.classList.add("menu__navbar--open");
 }
-function closeMenu () {    
+function closeMenu() {
     menuToggle.setAttribute("aria-expanded", "false");
     menuToggleIcon.src = "./assets/shared/icon-hamburger.svg";
     menuToggle.setAttribute("aria-label", "Abrir menu");
     menuNav.classList.remove("menu__navbar--open");
 }
 
-menuToggle.addEventListener("click", function() {
+menuToggle.addEventListener("click", function () {
     const expanded = menuToggle.getAttribute("aria-expanded");
     if (expanded === "false") {
         openMenu()
