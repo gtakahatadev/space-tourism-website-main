4418ba462c693adb87a7325a388a4602a1cb7795
gtakahatadev <gabrieltakahata.dev@gmail.com>
2026-09-09 16:03:32 -0300

    correcoes no estilo

--- js/destination.js
+++ js/destination.js
@@ -1,27 +1,27 @@
 const destinations = {
     moon: {
-        name: "Moon",
+        name: "MOON",
         webp: "./assets/destination/image-moon.webp",
         description: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
         distance: "384,400 km",
         travel: "3 days",
     },
     mars: {
-        name: "Mars",
+        name: "MARS",
         webp: "./assets/destination/image-mars.webp",
         description: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
         distance: "225 MIL. km",
         travel: "9 months",
     },
     europa: {
-        name: "Europa",
+        name: "EUROPA",
         webp: "./assets/destination/image-europa.webp",
         description: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
         distance: "628 MIL. km",
         travel: "3 years",
     },
     titan: {
-        name: "Titan",
+        name: "TITAN",
         webp: "./assets/destination/image-titan.webp",
         description: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
         distance: "1.6 BIL. km",
